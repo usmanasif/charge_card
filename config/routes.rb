e711c60d648c7a237b2174a5273d6fdc2da4de4e
Usman Asif <usmanasif@users.noreply.github.com>
2021-05-06 03:29:30 -0700

    Routes: Added routes for cards, transaciton_list and webhook endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :stripe, controller: :stripe, only: [] do
+    collection do
+      post :webhook
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      resources :cards, only: [:show, :index]
+      resources :transaction_lists, only: [:show, :index]
+    end
+  end
 end
